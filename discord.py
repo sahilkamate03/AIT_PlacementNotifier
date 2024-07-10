a923dd8b1b79373ba7f0c34d05fa8bc2d998df22
sahilkamate03 <45514385+sahilkamate03@users.noreply.github.com>
2024-07-11 01:24:37 +0530

    chore: Update scheduled query workflow to run every hour

--- discord.py
+++ discord.py
@@ -1,29 +1,65 @@
 import re
 import json
 import requests
+import random
 import markdownify
 from config import config
+
+emojis = ['🙇🏻','👴🏻','🫶🏻','👍🏻','😘','🤠','😉','😇','🤖','👾','🤡','😸','💫','🤩','🐻','🪿','🐼','🦄','🦬','🐮','🐶','🎉','🎊','🧸','🔥','🌞','🍀','🍁','🍄','🦊','🐛','🐣','😎']
+
+def break_paragraph(paragraph, max_length=1900):
+    paragraphs = []
+    current_paragraph = ""
+    current_length = 0
+
+    for line in paragraph.splitlines():
+        words = line.split()
+        for word in words:
+            word_length = len(word) + 1  # +1 for space
+            if current_length + word_length <= max_length:
+                current_paragraph += word + " "
+                current_length += word_length
+            else:
+                paragraphs.append(current_paragraph.strip())
+                current_paragraph = word + " "
+                current_length = word_length
+
+        current_paragraph += "\n"  # preserve line breaks
+
+    if current_paragraph.strip():  # append last paragraph
+        paragraphs.append(current_paragraph.strip())
+
+    return paragraphs
 
 
 def send_discord_message(message):
     webhook_url = config["WEBHOOK_URL"]
-    data = {"content": message}
-    headers = {"Content-Type": "application/json"}
+    # Discord limits messages to 2000 characters, so split the message into chunks
+    chunks = break_paragraph(message)
 
-    response = requests.post(webhook_url, data=json.dumps(data), headers=headers)
+    for i, chunk in enumerate(chunks):
+        data = {"content": chunk}
 
-    if response.status_code == 204:
-        print("Message sent successfully.")
-    else:
-        print(
-            f"Failed to send message. Status code: {response.status_code}, Response: {response.text}"
-        )
+        headers = {"Content-Type": "application/json"}
+
+        response = requests.post(webhook_url, data=json.dumps(data), headers=headers)
+
+        if response.status_code == 204:
+            print(f"Message part {i+1}/{len(chunks)} sent successfully.")
+        else:
+            print(
+                f"Failed to send message part {i+1}/{len(chunks)}. Status code: {response.status_code}, Response: {response.text}"
+            )
 
 
 def send_msg(response):
     data = response[0]["result"]["data"]
-    final_text = f"<@&{config['ROLE_ID']}> \n\n"
+    random_emoji = random.choice(emojis)
+    final_text = f"{random_emoji}\n\n"
+    final_text += f"<@&{config['ROLE_ID']}> \nNotice No: {config["PREV_NOTICES_COUNT"]}\n\n"
     final_text += f'**{data["title"]}**' + "\n\n"
+    send_discord_message(final_text)
+    final_text = ""
     html = data["body"]
     markdown_txt = markdownify.markdownify(html, heading_style="ATX")
     cleaned_text = re.sub(r"\n\s*\n", "\n\n", markdown_txt).strip()
